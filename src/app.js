1e27b9a2c5880558e73cabc50364f4004df7719f
Johan Wastring <jwr@SELUN-JWR-MAC1.lan>
2018-06-12 09:53:06 +0200

    with basic routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -13,17 +13,41 @@ const AddExpensePage = () => (
 const EditExpensePage = () => (
     <div>This is from my EditExpensePage</div>
 );
-const HelpPage = () => (
-    <div>This is from my HelpPage</div>
+const HelpPage = () => {
+        return <div>
+        This is from my HelpPage
+        </div>;
+};
+
+const NotFoundPage = () => (
+    <div>404 page! - <Link to="/">Go to home</Link>
+    
+    </div>
 );
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>  
+    </header>
+);
+
 const routes = (
     <BrowserRouter>
     <div>
+        <Header />
+        <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} exact={true}/>
         <Route path="/edit" component={EditExpensePage} exact={true}/>
         <Route path="/help" component={HelpPage} exact={true}/>
-    </div>  
+        <Route component={NotFoundPage} exact={true}/>
+    </Switch>  
+    </div>
+    
     </BrowserRouter>
 );
 
