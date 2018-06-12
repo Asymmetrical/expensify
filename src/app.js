60072093a15a0eaa3821b537fc1cb1aef7904e88
Johan Wastring <jwr@SELUN-JWR-MAC1.lan>
2018-06-12 09:01:04 +0200

    First commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>This is from my dahsboard</div>
+);
+const AddExpensePage = () => (
+    <div>This is from my AddExpensePage</div>
+);
+const EditExpensePage = () => (
+    <div>This is from my EditExpensePage</div>
+);
+const HelpPage = () => (
+    <div>This is from my HelpPage</div>
+);
+const routes = (
+    <BrowserRouter>
+    <div>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage} exact={true}/>
+        <Route path="/edit" component={EditExpensePage} exact={true}/>
+        <Route path="/help" component={HelpPage} exact={true}/>
+    </div>  
+    </BrowserRouter>
+);
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
